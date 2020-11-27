909f07cc15567b746d05c337d787f42929ba16b9
Janice <jwh@amazon.com>
2020-11-27 11:32:09 -0800

    build: use router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import LivingRoom from './components/LivingRoom';
+import elephant from './elephant.png';
+import React from "react";
+import {
+    BrowserRouter as Router,
+    Link,
+    Switch,
+    Route,
+} from "react-router-dom";
+
 
 function App() {
   return (
+      <Router className="App">
+          {/*<Link to="/livingRoom">*/}
+          {/*    <img src={elephant} className="logo" alt="elephant"/>*/}
+          {/*</Link>*/}
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <body className="App-body">
+      <Link to="/livingRoom">
+          <img src={elephant} className="logo" alt="elephant"/>
+      </Link>
+          <Switch>
+              {/*<Route path="/">*/}
+
+              {/*</Route>*/}
+              <Route path="/livingRoom">
+                  <LivingRoom url="http://localhost:3000/livingRoom"/>
+              </Route>
+              </Switch>
+      </body>
     </div>
+      </Router>
   );
 }
+
 
 export default App;
